3bc1b9a8c18dff1ffa506936bed8bd3d9738056c
Jamie Holroyde <jamie_holroyde@icloud.com>
2024-07-27 21:21:57 +0800

    introduction done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,11 @@ module.exports = {
     extend: {
       colors: {
         'main-bg': '#282C33',
-        'column-bg': '#3A3F47' // Slightly grayer shade
-      }
-    }
+        'column-bg': '#3A3F47',
+        'primary-text': '#FFFFFF', // Custom primary text color
+        'secondary-text': '#ABB2BF', // Custom secondary text color
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
