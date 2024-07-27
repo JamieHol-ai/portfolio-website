c44379ac98291a0abbe0014cc6b53e1aab7a8233
Jamie Holroyde <jamie_holroyde@icloud.com>
2024-07-27 21:43:09 +0800

    main layout for portfolio page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://jamiehol-ai.github.io',
   base: '/portfolio-website',
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
 });
