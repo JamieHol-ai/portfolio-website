14746dcefc2c70beef99b7d8dd3c5313ca677d14
Jamie Holroyde <jamie_holroyde@icloud.com>
2024-07-28 15:24:49 +0800

    about me done

---
+++ src/content/skillCategories.ts
@@ -0,0 +1,19 @@
+export const skillCategories = [
+    {
+      title: 'Languages',
+      skills: ['Python', 'JavaScript', 'Java', 'C++'],
+    },
+    {
+      title: 'Tools',
+      skills: ['Git', 'VS Code', 'Figma'],
+    },
+    {
+      title: 'Frameworks',
+      skills: ['Node.js', 'Astro', 'Tailwind'],
+    },
+    {
+      title: 'Other',
+      skills: ['HTML', 'CSS'],
+    },
+  ];
+  
